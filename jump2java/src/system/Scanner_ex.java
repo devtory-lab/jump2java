ae1349a34747731c66d13066d213f89cb824572b
an44ajh <an44ajh@gmail.com>
2020-06-16 17:42:03 +0900

    Scanner example

---
+++ jump2java/src/system/Scanner_ex.java
@@ -0,0 +1,14 @@
+package system;
+
+import java.util.Scanner;
+
+public class Scanner_ex {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(sc.next());
+		//System.err.print(sc.next());
+	}
+
+}
